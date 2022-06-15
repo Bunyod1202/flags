d0222d97ea245d4d74bc749e55b0e6de0b10a042
Bunyood <bunyodbunyod035@gmail.com>
2022-06-15 12:50:22 +0500

    bug fix

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function fon(){
+  let white = document.querySelector("body");
+  white.classList.toggle("black");
+  // if(white.classList.contains("red")){
+  //   white.classList.add("black");
+  //   white.classList.remove("red");
+  // }else {
+  //   white.classList.add("red");
+  //   white.classList.remove("black");   
+  // }
+                   
+}  
